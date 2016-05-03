328cced738ab5c784350a98e7064291c0bc367a9
Aldo Ziflaj <aldoziflaj95@gmail.com>
2016-05-03 11:20:42 +0200

    doesn't allow access on other companies departments

--- src/main/java/com/aziflaj/companies/action/SectorAction.java
+++ src/main/java/com/aziflaj/companies/action/SectorAction.java
@@ -2,25 +2,43 @@ package com.aziflaj.companies.action;
 
 import com.aziflaj.companies.data.dao.DepartmentDao;
 import com.aziflaj.companies.data.dao.SectorDao;
+import com.aziflaj.companies.data.model.Company;
+import com.aziflaj.companies.data.model.Department;
 import com.aziflaj.companies.data.model.Sector;
+import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.ActionSupport;
+import org.apache.struts2.StrutsStatics;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.sql.SQLException;
 import java.util.List;
 
-public class SectorAction extends ActionSupport {
+public class SectorAction extends ActionSupport implements StrutsStatics {
     private Long departmentId;
     private List<Sector> sectors;
 
     @Override
     public String execute() {
         try {
+            HttpServletRequest request = (HttpServletRequest) ActionContext.getContext()
+                    .get(HTTP_REQUEST);
+            HttpSession session = request.getSession();
+            Company company = (Company) session.getAttribute("company");
+
             DepartmentDao departmentDao = new DepartmentDao();
-            SectorDao sectorDao = new SectorDao();
-            sectors = sectorDao.getByDepartment(departmentDao.getById(departmentId));
+            Department department = departmentDao.getById(departmentId);
+
+            if (company.getId() == department.getCompany().getId()) {
+                SectorDao sectorDao = new SectorDao();
+                sectors = sectorDao.getByDepartment(department);
+            } else {
+                return "404";
+            }
 
         } catch (SQLException e) {
             e.printStackTrace();
+            return ERROR;
         }
         return SUCCESS;
     }
---
+++ src/main/webapp/WEB-INF/errors/404.jsp
@@ -0,0 +1,10 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Four Oh Four</title>
+</head>
+<body>
+<h1>Something went wrong and the resource can't be found</h1>
+:(
+</body>
+</html>
