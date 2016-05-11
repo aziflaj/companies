4719589ac9af1a9f0939a96705a4b00f750d11b8
Aldo Ziflaj <aldoziflaj95@gmail.com>
2016-05-11 11:43:24 +0200

    show all unassigned employees in other list

--- src/main/java/com/aziflaj/companies/action/employee/AllEmployeesAction.java
+++ src/main/java/com/aziflaj/companies/action/employee/AllEmployeesAction.java
@@ -6,16 +6,27 @@ import com.opensymphony.xwork2.ActionSupport;
 import org.apache.struts2.StrutsStatics;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class AllEmployeesAction extends ActionSupport implements StrutsStatics {
     public List<Employee> employees;
+    public List<Employee> unassigned;
 
     @Override
     public String execute() {
         try {
+            employees = new ArrayList<>();
+            unassigned = new ArrayList<>();
+
             EmployeeDao dao = new EmployeeDao();
-            employees = dao.all();
+            for (Employee e : dao.all()) {
+                if (e.getSector().getName().equals("sectorless")) {
+                    unassigned.add(e);
+                } else {
+                    employees.add(e);
+                }
+            }
             return SUCCESS;
         } catch (SQLException e) {
             e.printStackTrace();
@@ -25,5 +36,9 @@ public class AllEmployeesAction extends ActionSupport implements StrutsStatics {
 
     public List<Employee> getEmployees() {
         return employees;
+    }
+
+    public List<Employee> getUnassigned() {
+        return unassigned;
     }
 }
--- src/main/webapp/WEB-INF/jsp/employees/all.jsp
+++ src/main/webapp/WEB-INF/jsp/employees/all.jsp
@@ -7,6 +7,21 @@
 <body>
 List of employees
 <s:a action="create-employee-form" class="btn btn-primary">Add new employee</s:a>
+<h2>Unassigned</h2>
+<ul>
+    <s:iterator value="unassigned" var="u">
+        <li style="padding: 0.5em 0">
+            <s:property value="#u.fullName"/> is an unassigned <s:property value="#u.role.name"/>.
+
+            <s:a action="delete-employee" class="btn btn-danger">
+                <s:param name="employeeId" value="%{#e.id}"/>
+                Delete
+            </s:a>
+        </li>
+    </s:iterator>
+</ul>
+
+<h2>Other Employees</h2>
 <ul>
     <s:iterator value="employees" var="e">
         <li>
